6b84b769f629cef058159c81bd01cb7c93503cd4
Kylssor <santiagopte19@gmail.com>
2024-05-21 00:10:19 -0500

    Initial commit

--- Models/Roles/roles.py
+++ Models/Roles/roles.py
@@ -11,8 +11,7 @@ from Models.User.user import User
 class Roles(Base_Model, table=True):
 
     name: str = Field(default=None, nullable=False, max_length= 100)
-    user: uuid.UUID = Field(default=None, foreign_key="user.id")
-    rolesty: uuid.UUID = Field(default=None, nullable=False, foreign_key="rolesty.id")
+    rolesty_id: uuid.UUID = Field(default=None, nullable=False, foreign_key="rolesty.id")
 
 
     roles_Ty: Optional[RolesTy] = Relationship(back_populates="roles")
